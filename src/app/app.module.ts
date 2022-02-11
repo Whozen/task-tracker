bf705ba0d183a5616160cd58dbfa4c6ed652cdd8
Ujjen Bania <ujjenms.bania@gmail.com>
2022-02-11 16:48:21 -0600

    form and ui functionalities added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componenets/header/header.component';
 import { ButtonsComponent } from './componenets/buttons/buttons.component';
 import { TasksComponent } from './componenets/tasks/tasks.component';
 import { TaskItemComponent } from './componenets/task-item/task-item.component';
+import { AddTaskComponent } from './componenets/add-task/add-task.component';
+import { AboutComponent } from './componenets/about/about.component';
+import { FooterComponent } from './componenets/footer/footer.component';
+
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent}
+]
 
 @NgModule({
   declarations: [
@@ -15,12 +25,17 @@ import { TaskItemComponent } from './componenets/task-item/task-item.component';
     HeaderComponent,
     ButtonsComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
