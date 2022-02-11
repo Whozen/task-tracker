88734265400481654f652ddd0a82c69994ded1f8
Ujjen Baniya <ujjen263@mail.fresnostate.edu>
2022-02-11 12:04:25 -0600

    first upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componenets/header/header.component';
+import { ButtonsComponent } from './componenets/buttons/buttons.component';
+import { TasksComponent } from './componenets/tasks/tasks.component';
+import { TaskItemComponent } from './componenets/task-item/task-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonsComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-task.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task';
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Wake up',
+        day: 'May 12 at 6:00 am',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Daily Meeting',
+        day: 'May 12 at 9:00 am',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Workout',
+        day: 'May 14 at 6:00 pm',
+        reminder: true,
+    },
+];
